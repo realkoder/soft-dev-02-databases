f3e02ad1fba955d3435b851a00496eefc1a37c21
realkoder <alexanderbtcc@gmail.com>
2025-09-16 17:41:03 +0200

    renamed to V1 and back to v1

--- munchora-server-relational-db/spec/requests/api/v1/feedbacks_spec.rb
+++ munchora-server-relational-db/spec/requests/api/v1/feedbacks_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Feedbacks", type: :request do
+RSpec.describe "Api::v1::Feedbacks", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
--- munchora-server-relational-db/spec/requests/api/v1/grocery_lists_spec.rb
+++ munchora-server-relational-db/spec/requests/api/v1/grocery_lists_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::GroceryLists", type: :request do
+RSpec.describe "Api::v1::GroceryLists", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
