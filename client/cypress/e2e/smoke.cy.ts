797552b8d683c59b6ccc0902c3229022f5b1c4e0
realkoder <alexanderbtcc@gmail.com>
2025-09-30 18:41:40 +0200

    reject hydration issues with cypress

--- client/cypress/e2e/smoke.cy.ts
+++ client/cypress/e2e/smoke.cy.ts
@@ -3,5 +3,7 @@ describe("Smoke test", () => {
         cy.visit("/");
         cy.wait(1000);
         cy.contains("Sign In").should("exist");
+
+        cy.contains("Sign In").click();
     });
 });
