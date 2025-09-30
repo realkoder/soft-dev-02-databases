b11361c4d8d957836d149c32976797cf20cfa6cd
realkoder <alexanderbtcc@gmail.com>
2025-09-30 18:33:17 +0200

    removed beforeeach hook

--- client/cypress/e2e/smoke.cy.ts
+++ client/cypress/e2e/smoke.cy.ts
@@ -1,9 +1,4 @@
 describe("Smoke test", () => {
-    beforeEach(() => {
-        cy.viewport(1280, 720);
-        Object.defineProperty(window.navigator, 'language', { value: 'en-US' });
-    });
-
     it("loads the home page", () => {
         cy.visit("/");
         cy.wait(1000);
