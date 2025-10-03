984bdccf3641e4fd0d225e905a1b70c77fa426e1
realkoder <alexanderbtcc@gmail.com>
2025-10-03 19:36:33 +0200

    fixed paths trggering e2e CI - commented away recipe tests since AI can be expensive to run everytime xD

--- client/cypress/e2e/scenarios/recipes/recipeLoadFromGenerateRecipePage.cy.ts
+++ client/cypress/e2e/scenarios/recipes/recipeLoadFromGenerateRecipePage.cy.ts
@@ -10,27 +10,27 @@ describe('Load recipe from recipes', () => {
     cy.loginOrSignUpByApi();
   });
 
-  it('should create pancake recipe from recipeGenerator', () => {
-    cy.loadPage('recipes');
-
-    cy.contains('span', 'AI Create').click();
-
-    cy.get('textarea').type(
-      'Generate a simple, easy-to-make pancake recipe that is fluffy and delicious, including suggested toppings such as fruits, syrup, chocolate, or nuts, with clear step-by-step instructions suitable for beginners.'
-    );
-
-    cy.get('button[cy-data="generate-recipe"]').click();
-
-    cy.wait(20_000); // AI generation does take some time
-
-    cy.checkPageLoadedCorrectly('recipeUpdate');
-  });
-
-  it('should display recipe when first recipeCard is clicked', () => {
-    cy.loadPage('recipes');
-
-    cy.get('[data-cy="recipe-card-link"]').first().click();
-
-    cy.checkPageLoadedCorrectly('recipe');
-  });
+  // it('should create pancake recipe from recipeGenerator', () => {
+  //   cy.loadPage('recipes');
+  //
+  //   cy.contains('span', 'AI Create').click();
+  //
+  //   cy.get('textarea').type(
+  //     'Generate a simple, easy-to-make pancake recipe that is fluffy and delicious, including suggested toppings such as fruits, syrup, chocolate, or nuts, with clear step-by-step instructions suitable for beginners.'
+  //   );
+  //
+  //   cy.get('button[cy-data="generate-recipe"]').click();
+  //
+  //   cy.wait(20_000); // AI generation does take some time
+  //
+  //   cy.checkPageLoadedCorrectly('recipeUpdate');
+  // });
+  //
+  // it('should display recipe when first recipeCard is clicked', () => {
+  //   cy.loadPage('recipes');
+  //
+  //   cy.get('[data-cy="recipe-card-link"]').first().click();
+  //
+  //   cy.checkPageLoadedCorrectly('recipe');
+  // });
 });
