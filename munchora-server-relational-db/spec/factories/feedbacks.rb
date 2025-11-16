19a6590cee7bf4a2a67bf4b128b0faa7ae83db45
realkoder <alexanderbtcc@gmail.com>
2025-11-16 14:10:33 +0100

    remove unused specs

--- munchora-server-relational-db/spec/factories/feedbacks.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :feedback do
-    message { "Great app - please add tracking for calories, protein, vitamine etc." }
-    name { "Alexander Christensen" }
-    category { "features" }
-    email { "alexander@gmail.com" }
-  end
-end
--- munchora-server-relational-db/spec/factories/recipe_suggestions.rb
+++
@@ -1,4 +0,0 @@
-FactoryBot.define do
-  factory :recipe_suggestion do
-  end
-end
